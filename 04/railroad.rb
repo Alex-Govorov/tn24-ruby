75d16beac07084dde1f55d51d331c86ba8b8d390
alex <alex@tn24.tn24.virtualbox.org>
2024-02-16 08:46:55 +0300

    Интерфейсы, наследование и полиморфизм

---
+++ 04/railroad.rb
@@ -0,0 +1,165 @@
+class Railroad
+  require_relative('cargo_carriage')
+  require_relative('cargo_train')
+  require_relative('passenger_carriage')
+  require_relative('passenger_train')
+  require_relative('route')
+  require_relative('station')
+
+  MENU_METHODS = { 'Создать станцию' => 'create_station',
+                   'Создать поезд' => 'create_train',
+                   'Создать маршрут и управлять станциями в нем (добавлять, удалять)' => 'create_route',
+                   'Назначить маршрут поезду' => 'assign_route_to_train',
+                   'Добавить вагоны к поезду' => 'add_carriages_to_train',
+                   'Отцепить вагоны от поезда' => 'release_carriages_from_train',
+                   'Переместить поезд по маршруту вперед и назад' => 'move_train',
+                   'Просмотреть список станций и список поездов на станции' => 'station_trains',
+                   'Выйти' => 'exit' }.freeze
+
+  def menu
+    MENU_METHODS.each_with_index { |(key, _value), index| puts "#{index}. #{key}" }
+    send(MENU_METHODS.values[input_i])
+  end
+
+  private
+
+  # Все методы, что ниже применяются исключительно текущим классом поэтому private
+  def create_station
+    puts 'Введите название станции:'
+    name = gets.chomp
+    (@stations ||= []) << Station.new(name)
+    menu
+  end
+
+  def create_train
+    puts '0: грузовой, 1: пассажирский'
+    case input_i
+    when 0
+      puts 'Номер поезда:'
+      (@trains ||= []) << CargoTrain.new(input_str)
+    when 1
+      puts 'Номер поезда:'
+      (@trains ||= []) << PassengerTrain.new(input_str)
+    end
+    menu
+  end
+
+  def create_route
+    puts 'Начальная станция:'
+    station_first = choose_station
+    puts 'Конечная станция:'
+    station_last = choose_station
+    route = Route.new(station_first, station_last)
+    (@routes ||= []) << route
+    puts 'Управлять станциями в маршруте? 0: Да / 1: Нет'
+    manage_stations_in_route(route) if input_i.zero?
+    menu
+  end
+
+  def assign_route_to_train
+    train = choose_train
+    puts 'Выберите маршрут:'
+    routes_list
+    route = @routes[input_i]
+    train.get_route(route)
+    menu
+  end
+
+  def add_carriages_to_train
+    train = choose_train
+    puts 'Сколько вагонов?'
+    carriages_count = input_i
+    puts 'Какие вагоны цепляем: 0. грузовые / 1. пассажирские'
+    case input_i
+    when 0
+      carriages_count.times { train.take_carriage(CargoCarriage.new) }
+    when 1
+      carriages_count.times { train.take_carriage(PassenegerCarriage.new) }
+    end
+    menu
+  end
+
+  def release_carriages_from_train
+    train = choose_train
+    puts 'Сколько вагонов?'
+    carriages_count = input_i
+    carriages_count.times { train.release_carriage(train.carriages.last) } if carriages_count <= train.carriages.count
+    menu
+  end
+
+  def move_train
+    train = choose_train
+    puts '0: Ехать вперед / 1: Ехать назад'
+    case input_i
+    when 0
+      train.move_to_next_station
+      puts 'Конечная' unless train.next_station
+    when 1
+      train.move_to_previous_station
+      puts 'Конечная' unless train.previous_station
+    end
+    menu
+  end
+
+  def station_trains
+    puts 'Выберите станцию:'
+    stations_list
+    station = @stations[input_i]
+    station.trains.each { |train| puts "#{train.number} #{train.type}" }
+    menu
+  end
+
+  def choose_train
+    puts 'Выберите поезд:'
+    trains_list
+    @trains[input_i]
+  end
+
+  def choose_station
+    puts 'Выберите станцию:'
+    stations_list
+    @stations[input_i]
+  end
+
+  def manage_stations_in_route(route)
+    puts '0:Добавить станцию / 1: Удалить станцию'
+    case input_i
+    when 0
+      add_station_in_route(route)
+    when 1
+      delete_station_from_route(route)
+    end
+  end
+
+  def add_station_in_route(route)
+    puts 'Какую станцию добавляем?'
+    stations_list
+    route.add_station(@stations[input_i])
+  end
+
+  def delete_station_from_route(route)
+    puts 'Какую станцию удаляем?'
+    route.stations.each_with_index(station, index) { |station, index| puts "#{index}. #{station.name}" }
+    route.delete_station(@stations[input_i])
+  end
+
+  def stations_list
+    @stations.each_with_index { |station, index| puts "#{index}. #{station.name}" }
+  end
+
+  def routes_list
+    @routes.each_with_index { |route, index| puts "#{index}. #{route.stations}" }
+  end
+
+  def trains_list
+    @trains.each_with_index { |train, index| puts "#{index}. #{train.number}" }
+  end
+
+  def input_i
+    gets.chomp.to_i
+  end
+
+  def input_str
+    gets.chomp
+  end
+end
---
+++ 04/train.rb
@@ -0,0 +1,63 @@
+class Train
+  attr_reader :speed, :carriages, :number
+
+  def initialize(number)
+    @number = number
+    @speed = 0
+    @carriages = []
+  end
+
+  def speed_up(speed)
+    @speed = speed
+  end
+
+  def stop
+    @speed = 0
+  end
+
+  def take_carriage(carriage)
+    @carriages << carriage
+  end
+
+  def release_carriage(carriage)
+    @carriages.delete(carriage)
+  end
+
+  def get_route(route)
+    @route = route
+    @current_station_index = route.stations.index(route.stations.first)
+    current_station.take_train(self)
+  end
+
+  def move_to_next_station
+    return unless next_station
+
+    current_station.send_train(self)
+    @current_station_index += 1
+    current_station.take_train(self)
+  end
+
+  def move_to_previous_station
+    return unless previous_station
+
+    current_station.send_train(self)
+    @current_station_index -= 1
+    current_station.take_train(self)
+  end
+
+  protected
+
+  # Методы, что нижt используются классами потомками CargoTrain и PassengerTrain, поэтому protected
+
+  def current_station
+    @route.stations[@current_station_index]
+  end
+
+  def next_station
+    @route.stations[@current_station_index + 1]
+  end
+
+  def previous_station
+    @route.stations[@current_station_index - 1] unless @current_station_index.zero?
+  end
+end
